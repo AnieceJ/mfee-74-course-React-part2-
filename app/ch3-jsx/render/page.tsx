4f02b3865cea81fda91a328b4b729cf0b6867524
yisichoco <yisichoco@gmail.com>
2026-05-28 10:54:48 +0800

    JSX渲染範例-數字、字串

--- app/ch3-jsx/render/page.tsx
+++ app/ch3-jsx/render/page.tsx
@@ -3,7 +3,15 @@
 export default function RenderPage() {
   return (
     <>
-      <div>Render Page</div>
+      <h1>JSX渲染(render)範例</h1>
+      <hr />
+      <h2>數字number</h2>
+      {1 - 1}
+      {NaN}
+      <h2>字串string</h2>
+      這是字串
+      {'這也是字串'}
+      {`這也是${1 + 1}字串`}
     </>
   );
 }
