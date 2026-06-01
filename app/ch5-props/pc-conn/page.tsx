b352bc271166f536f42a664fac3e890c38b1e4a7
yisichoco <yisichoco@gmail.com>
2026-06-01 16:20:18 +0800

    Props屬性-PC連結-結構

---
+++ app/ch5-props/pc-conn/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Parent from './_components/parent';
+
+export default function PcPage() {
+  return (
+    <>
+      <h1>Props屬性範例: P-C連結</h1>
+      <hr />
+      <Parent />
+    </>
+  );
+}
