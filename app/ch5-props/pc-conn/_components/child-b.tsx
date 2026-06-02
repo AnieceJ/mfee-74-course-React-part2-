bccdaaa9201a81e63a9ea28d454377da4bf060ce
yisichoco <yisichoco@gmail.com>
2026-06-02 09:36:59 +0800

    Props屬性-PC連結-C到C傳遞資料

--- app/ch5-props/pc-conn/_components/child-b.tsx
+++ app/ch5-props/pc-conn/_components/child-b.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 // props
-interface ChildBProps {}
+interface ChildBProps {
+  pData: string;
+}
 
-export default function ChildB({}: ChildBProps) {
+export default function ChildB({ pData = '' }: ChildBProps) {
   return (
     <>
       <div>ChildB(子女元件B)</div>
+      <p>來自子女元件A的資料: {pData}</p>
     </>
   );
 }
--- app/ch5-props/pc-conn/_components/parent.tsx
+++ app/ch5-props/pc-conn/_components/parent.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 
 import ChildA from './child-a';
-// import ChildB from './child-b';
+import ChildB from './child-b';
 
 export default function Parent() {
   // 宣告一組狀態，讓子女A回傳資料到這元件使用
@@ -13,10 +13,12 @@ export default function Parent() {
     <>
       <h2>Parent(父母元件)</h2>
       <hr />
-      <p>來自子女元件A的資料: {pData}</p>
+      {/* <p>來自子女元件A的資料: {pData}</p> */}
       {/* C -> P範例 */}
       <ChildA setPData={setPData} />
-      {/* <ChildB /> */}
+      <hr />
+      {/* C -> C範例 */}
+      <ChildB pData={pData} />
     </>
   );
 }
