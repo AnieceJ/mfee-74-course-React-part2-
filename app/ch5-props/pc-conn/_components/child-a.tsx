d3b26daeedbce778d42f38670a505c651a6c7059
yisichoco <yisichoco@gmail.com>
2026-06-02 09:27:14 +0800

    Props屬性-PC連結-C到P傳遞

--- app/ch5-props/pc-conn/_components/child-a.tsx
+++ app/ch5-props/pc-conn/_components/child-a.tsx
@@ -1,12 +1,25 @@
 'use client';
 
 // props
-interface ChildAProps {}
+interface ChildAProps {
+  setPData: (s: string) => void;
+}
 
-export default function Child({}: ChildAProps) {
+export default function ChildA({ setPData = () => {} }: ChildAProps) {
+  const cData = '子女a內部資料';
+
   return (
     <>
       <div>ChildA(子女元件A)</div>
+      <button
+        onClick={() => {
+          // 子女中得到父母傳來的設定狀態的方法，呼叫後回傳資料回父母元件
+          // set狀態的方法因為有副作用，只能在事件處理函式或useEffect鉤子中呼叫
+          setPData(cData);
+        }}
+      >
+        送資料到父母元件
+      </button>
     </>
   );
 }
