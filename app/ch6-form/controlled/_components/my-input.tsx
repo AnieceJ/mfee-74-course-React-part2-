423ca69a0468cc5b9e1ebefa251e44d310847f38
yisichoco <yisichoco@gmail.com>
2026-06-08 13:57:29 +0800

    Form&Refs表單_可控表單元件-架構

---
+++ app/ch6-form/controlled/_components/my-input.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function MyInput() {
+  return (
+    <>
+      <h2>文字輸入框類</h2>
+      <input type="text" />
+    </>
+  );
+}
---
+++ app/ch6-form/controlled/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import MyInput from './_components/my-input';
+
+export default function ControlledPage() {
+  return (
+    <>
+      <h1>可控表單元件範例(controlled)</h1>
+      <hr />
+      <MyInput />
+    </>
+  );
+}
