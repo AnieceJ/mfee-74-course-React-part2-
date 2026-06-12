ae42febe4d55ffdd6cb7b6cd1764fbae0bd8982d
Eddy Chang <eddy@joomla.com.tw>
2026-06-12 16:28:26 +0800

    API 路由範例

---
+++ app/api/blogs/[id]/route.ts
@@ -0,0 +1,50 @@
+import db from '@/config/mysql';
+
+// api路由: /api/blogs/[id]
+
+// GET 所有資料
+export async function GET(request, { params }) {
+  // 從動態路由參數得到id(對應資料夾名稱[id])
+  // 需要轉成數字資料類型
+  const id = Number((await params).id);
+  // 資料庫查詢得到資料
+  const [blogs] = await db.query(`SELECT * FROM blog WHERE id=${id}`);
+  console.log(blogs);
+  // 只需要1筆資料
+  const blog = blogs[0];
+  // 回應
+  return Response.json({ status: 'success', data: { blog } });
+}
+
+// (PUT)更新資料
+export async function PUT(request, { params }) {
+  // 從動態路由參數中得到id (對應資料夾名稱)
+  // 需要轉成數字資料類型
+  const id = Number((await params).id);
+  // 從request得到body資料
+  const body = await request.json();
+  console.log(body);
+
+  // 從body得到title, content資料
+  const { title, content } = body;
+  // 執行查詢
+  const [result] = await db.query(
+    `UPDATE blog SET title = '${title}', content = '${content}' WHERE id = ${id};`
+  );
+  console.log(result);
+
+  return Response.json({ status: 'success', data: null });
+}
+
+// (DELETE)刪除資料
+export async function DELETE(request, { params }) {
+  // 從動態路由參數中得到id (對應資料夾名稱)
+  // 需要轉成數字資料類型
+  const id = Number((await params).id);
+
+  // 執行查詢
+  const [result] = await db.query(`DELETE FROM blog WHERE id = ${id};`);
+  console.log(result);
+
+  return Response.json({ status: 'success', data: null });
+}
---
+++ app/api/blogs/route.ts
@@ -0,0 +1,28 @@
+import db from '@/config/mysql';
+
+// api路由: /api/blogs
+
+// GET 所有資料
+export async function GET(request, { params }) {
+  // 資料庫查詢得到資料
+  const [blogs] = await db.query(`SELECT * FROM blog`);
+  console.log(blogs);
+  // 回應
+  return Response.json({ status: 'success', data: { blogs } });
+}
+
+// POST 新增用
+export async function POST(request, { params }) {
+  // 得到前端傳來的資料
+  const body = await request.json();
+  console.log(body);
+  // 解構出title, content
+  const { title, content } = body;
+  // 資料庫查詢-新增資料
+  const [result] = await db.query(
+    `INSERT INTO blog (title, content, updated_at) VALUES ('${title}', '${content}', NOW())`
+  );
+  console.log(result);
+  // 回應
+  return Response.json({ status: 'success', data: null });
+}
